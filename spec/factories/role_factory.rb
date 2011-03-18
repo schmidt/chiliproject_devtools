c97c0f446562ba7ff59b2a973a3c5b78f85a15f9
Jens Ulferts <j.ulferts@finn.de>
2011-03-18 15:24:01 +0100

    additional role factories

--- spec/factories/role_factory.rb
+++ spec/factories/role_factory.rb
@@ -4,8 +4,14 @@ Factory.define :role do |r|
   r.assignable true
 end
 
-Factory.define :non_member, :class => Role do |r|
+Factory.define :non_member, :parent =>:role do |r|
   r.name "Non member"
   r.builtin Role::BUILTIN_NON_MEMBER
-  r.assignable true
+  r.assignable false
+end
+
+Factory.define :anonymous_role, :parent => :role do |r|
+  r.name "Anonymous"
+  r.builtin Role::BUILTIN_ANONYMOUS
+  r.assignable false
 end
