6ed32e76a756f3306bba83640e1856870f4e5a8e
Gregor Schmidt <ruby@schmidtwisser.de>
2011-10-25 13:06:09 +0200

    Better looking names in project factory

--- spec/factories/project_factory.rb
+++ spec/factories/project_factory.rb
@@ -1,5 +1,5 @@
 Factory.define(:project) do |p|
-  p.sequence(:name) { |n| "My#{n}" }
-  p.sequence(:identifier) { |n| "myproject#{n}" }
+  p.sequence(:name) { |n| "My Project No. #{n}" }
+  p.sequence(:identifier) { |n| "myproject_no_#{n}" }
   p.enabled_module_names Redmine::AccessControl.available_project_modules
 end
