6e959e8b1b0c078c90a9d7863d514035836ffa46
Gregor Schmidt <ruby@schmidtwisser.de>
2011-10-25 13:06:27 +0200

    Version names need to be uniq

--- spec/factories/version_factory.rb
+++ spec/factories/version_factory.rb
@@ -1,5 +1,5 @@
 Factory.define :version do |v|
-  v.name "version"
+  v.sequence(:name) { |i| "Version #{i}" }
   v.effective_date Date.today + 14.days
   v.association :project, :factory => :project
 end
